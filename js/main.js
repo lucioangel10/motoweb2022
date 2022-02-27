c1391ca518f30242a7749017cd9e95a6d1605b24
lucioangel10 <96026330+lucioangel10@users.noreply.github.com>
2022-02-27 14:21:05 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,27 +12,43 @@ class Simulador {
         this.resultado1 = this.precio - this.anticipo;
         this.resultado1 = this.resultado1 * tazasInteres[0];
         this.resultado1 = this.resultado1 / 12;
+        (this.resultado1).toFixed();
+
     }
     cuotas18() {
         this.resultado2 = this.precio - this.anticipo;
         this.resultado2 = this.resultado2 * tazasInteres[1];
         this.resultado2 = this.resultado2 / 18;
+        (this.resultado2).toFixed();
+
     }
     cuotas24() {
         this.resultado3 = this.precio - this.anticipo;
         this.resultado3 = this.resultado3 * tazasInteres[2];
         this.resultado3 = this.resultado3 / 24;
     }
-    mostrar() {
+    async mostrar() {
         
 
         
         this.cuotas12();
         this.cuotas18();
         this.cuotas24();
-        alert(`La financiacion para el importe y anticipo elejido es:\n12 cuotas de $${this.resultado1}\n18 cuotas de $${this.resultado2}\n24 cuotas de $${this.resultado3}`);
+        await Swal.fire({
+            icon: 'success',
+            title:`La financiacion para el importe y anticipo elejido es:`,
+            text: `12 cuotas de $${parseInt(this.resultado1)}\n - 18 cuotas de $${parseInt(this.resultado2)}\n - 24 cuotas de $${parseInt(this.resultado3)}`,
+            footer: '<a href="sucursales.html">Visitar una sucursal</a>'
+          })
+
     }
 }
+
+
+
+
+
+
 class Moto {
     constructor(nombre, precio, tipo, marca, img, id, dolar) {
         this.nombre = nombre.toUpperCase();
@@ -43,15 +59,27 @@ class Moto {
         this.id=id;
         this.dolar=dolar;
         this.conversion;
+        this.aux;
         
 
     }
-    importada(dolar) {
-       
+    async importada(dolar) {
         if (this.dolar===true){
+            this.aux=this.precio;  
+            this.precio= this.precio*dolar;
+            await Swal.fire({
+                icon: 'info',
+                title: 'La moto es importada!',
+                text: `Cambiamos su precio a pesos, con un dolar de $${dolar}`,
+              })
+        
             
-            alert(`El precio de la moto elegida es el dolares. Para financiar pasamos el precio a pesos.\nLa cotizacion del dia en dolares es: $${dolar}`)
-            this.conversion= this.precio*dolar;
+        }
+      
+    }
+    volverPrecio(){
+        if (this.dolar===true){
+            this.precio=this.aux;
         }
     }
 }
@@ -59,6 +87,10 @@ class Moto {
 
 
 // variables
+
+
+
+
 let exito;
 let exitoEnLS = parseInt(localStorage.getItem('simulaciones'));
 if(exitoEnLS){
@@ -69,7 +101,7 @@ if(exitoEnLS){
 localStorage.setItem('exito', exito);
 const caracterNum=contarNum(0,9)
 let anticipo;
-const dolar=[215,213,210,226,230,]
+const dolar=[215,213,210,226,230]
 const tazasInteres = [1.25, 1.35, 1.45]
 const motos=[];
 const cantidadSimulaciones= document.querySelector('.simulaciones');
@@ -80,6 +112,7 @@ const contenedorTipo=document.querySelector('.contenedor-tipos');
 agregarMotos(motos);
 
 mezclarMotos(motos);
+mostrarMotos(motos);
 
 
 mostrarBotones();
@@ -93,23 +126,34 @@ function mostrarBotones(){
     const btnStreet = document.createElement('button');
     btnStreet.classList.add('btn-filtro');
     btnStreet.textContent='Street';
-    btnStreet.onclick = ()=> {mostrarMotos(motos.filter((moto)=> moto.tipo.includes('STREET')))}
+    btnStreet.onclick = ()=> {
+        mezclarMotos(motos);
+
+        mostrarMotos(motos.filter((moto)=> moto.tipo.includes('STREET')))}
     const btnOnoff = document.createElement('button');
     btnOnoff.classList.add('btn-filtro');
     btnOnoff.textContent='On/Off';
-    btnOnoff.onclick = ()=> {mostrarMotos(motos.filter((moto)=> moto.tipo.includes('ON/OFF')))}
+    btnOnoff.onclick = ()=> {
+        mezclarMotos(motos);
+        mostrarMotos(motos.filter((moto)=> moto.tipo.includes('ON/OFF')))}
     const btnTodas = document.createElement('button');
     btnTodas.classList.add('btn-filtro');
     btnTodas.textContent='Todas';
-    btnTodas.onclick = ()=> {mostrarMotos(motos)}
+    btnTodas.onclick = ()=> {
+        mezclarMotos(motos);
+        mostrarMotos(motos)}
     const btnScooter = document.createElement('button');
     btnScooter.classList.add('btn-filtro');
     btnScooter.textContent='Scooter';
-    btnScooter.onclick = ()=> {mostrarMotos(motos.filter((moto)=> moto.tipo.includes('SCOOTER')))}
+    btnScooter.onclick = ()=> {
+        mezclarMotos(motos);
+        mostrarMotos(motos.filter((moto)=> moto.tipo.includes('SCOOTER')))}
     const btnCub = document.createElement('button');
     btnCub.classList.add('btn-filtro');
     btnCub.textContent='Cub';
-    btnCub.onclick = ()=> {mostrarMotos(motos.filter((moto)=> moto.tipo.includes('CUB')))}
+    btnCub.onclick = ()=> {
+        mezclarMotos(motos);
+        mostrarMotos(motos.filter((moto)=> moto.tipo.includes('CUB')))}
     contenedorTipo.appendChild(btnStreet);
     contenedorTipo.appendChild(btnOnoff);
     contenedorTipo.appendChild(btnTodas);
@@ -153,7 +197,10 @@ function mostrarMotos(arrayMoto){
         const btnSimu= document.createElement('button')
         btnSimu.classList.add('btn-simulador');
         btnSimu.textContent='Simular cuotas';
-        btnSimu.onclick = ()=>{simularCuotas(moto)};
+        btnSimu.onclick = async ()=>{
+            await moto.importada(dolar[Math.floor(Math.random()*5)]);
+                         
+            pedirAnticipo(moto)};
         
         divProducto.appendChild(tipoProducto);
         divProducto.appendChild(imgProducto);
@@ -176,10 +223,42 @@ function mostrarMotos(arrayMoto){
 
 
 
-function pedirAnticipo(precio, marca, nombre) {
+async function pedirAnticipo(moto) {
+    
 
-    anticipo = parseFloat(prompt(`Seleccionaste ${marca} ${nombre}\nEl precio es de $${precio}.\nSi desea entregar un anticipo ingreseló, sino ingrese "0"`));
-}
+    const { value: anticipo } = await Swal.fire({
+        title: `Haz seleccionado <strong>${moto.marca} ${moto.nombre}.</strong>`,
+        text: `Su precio es $${moto.precio}.`,
+        icon: 'success',
+        input: 'range',
+        inputLabel: 'Ingresa tu anticipo.',
+        inputAttributes: {
+            min: 0,
+            max: moto.precio,
+            step: 1000
+        },
+        inputValue: 0,
+        
+        showCloseButton: true,
+        
+    })
+            if (anticipo) {
+            let simulador;
+            simulador = new Simulador(moto.precio, anticipo);
+            simulador.mostrar();
+    
+            exito++;
+            contarSimulaciones(exito);
+            mostrarSimulaciones();   
+            moto.volverPrecio();     
+
+
+        }
+        
+        
+    
+    }
+
 function malAnticipo(anticipo, precio) {
     if (anticipo >= precio || isNaN(anticipo) || anticipo < 0) {
 
@@ -195,47 +274,12 @@ function contarNum(inicio, fin) {
     }
     return arrAux;
 }
-function simularCuotas(moto) {
-    moto.importada(dolar[Math.floor(Math.random()*5)]);
 
-    for (let i = 1; i > 0; i++) {
-        if(moto.dolar===true){
-            pedirAnticipo(moto.conversion, moto.marca, moto.nombre);
-
-        }else{
-            pedirAnticipo(moto.precio, moto.marca, moto.nombre);
-
-        }
-        if(moto.dolar===true){
-            if(malAnticipo(anticipo, moto.conversion)) {
-                alert('Su anticípo no es válido.\nLos carácteres disponibles para esta opcion son:\n' + caracterNum.join(", "));
-                continue;
-            }
-        }else if(malAnticipo(anticipo, moto.precio)) {
-            alert('Su anticípo no es válido.\nLos carácteres disponibles para esta opcion son:\n' + caracterNum.join(", "));
-            continue;
-        }
-        break;
-    }
-    let simulador;
-    if(moto.dolar===true){
-        simulador = new Simulador(moto.conversion, anticipo);
-
-    }else{
-        simulador = new Simulador(moto.precio, anticipo);
-
-    }
-    simulador.mostrar();
-    
-    exito++;
-    contarSimulaciones(exito);
-    mostrarSimulaciones();
-}
 function agregarMotos(array){
 array.push(new Moto('Titan', '460000', 'street', 'honda', 'multimedia/productos/street/ym20.png',1, false),
 new Moto('cb 250', '690000', 'street', 'honda','multimedia/productos/street/cb250-street_ccexpress.png', 2,false),
 new Moto('glh 150', '315000', 'street', 'honda', 'multimedia/productos/street/glh-street_ccexpress.png',3,false),
-new Moto('mt-03', '9500', 'street', 'yamaha','multimedia/productos/street/mt03-street_ccexpress.png',4,true),
+new Moto('mt-03', '10000', 'street', 'yamaha','multimedia/productos/street/mt03-street_ccexpress.png',4,true),
 new Moto('fzfi d', '542000', 'street', 'yamaha','multimedia/productos/street/fz-street_ccexpress.png',5,false),
 new Moto('ybr 125', '412000', 'street', 'yamaha','multimedia/productos/street/ybr-calle_ccexpress.png',6,false),
 new Moto('crypton 110', '334600', 'cub', 'yamaha','multimedia/productos/cub/t110-cub_ccexpress.png',7,false),
@@ -264,7 +308,13 @@ new Moto('wave 110', '220000', 'cub', 'honda','multimedia/productos/cub/wave-cub
 
 function mostrarSimulaciones(){
     const simulacionesAlmacenadas=localStorage.getItem('simulaciones')
-    cantidadSimulaciones.textContent= `Ya se han realizaron ${simulacionesAlmacenadas} simulaciones exitozas`;
+    if(simulacionesAlmacenadas){
+        cantidadSimulaciones.textContent= `Ya se han realizado ${simulacionesAlmacenadas} simulaciones exitozas`;
+
+    }else{
+        cantidadSimulaciones.textContent= `Ya se han realizado ${0} simulaciones exitozas`;
+
+    }
 }
 function guardarMotos(clave, valor){
     localStorage.setItem(clave,valor);
